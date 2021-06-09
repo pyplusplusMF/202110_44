03ab2c3bdeac46c543d6208e364e277c7edc0176
pyplusplusMF <>
2021-06-09 00:58:20 +0000

    semana 06 ejemplo reto 5

--- _44semana06clase18EjemploReto5.py
+++ _44semana06clase18EjemploReto5.py
@@ -1,2 +1,38 @@
-# clase del martes 8 de junio
-# tema: Ejemplo del reto 5
+# Clase del martes 8 de junio
+# Ejemplo del Reto 5
+# Lectura del archivo titanic3.csv y cargado en un DataFrame
+
+def funcionEjemplo (nombreArchivo:str) -> dict:
+    import pandas as pd
+    # archivos
+    # 1. Cargar el archivo titanic en un dataframe
+    dataFrame = pd.read_csv(nombreArchivo)
+    # print (dataFrame)
+    # dataFrame.info()
+    nombres = dataFrame['name']
+    # print ('nombres: ')
+    # print (nombres)
+
+    mayorEdad = max ( dataFrame['age'])
+    # print ('La edad mayor es: ', mayorEdad, 'años')
+    # La edad mayor es:  80.0 años
+
+    menorEdad = min ( dataFrame['age'])
+    # print ('La edad menor es: ', menorEdad, 'años')
+    # La edad menor es:  0.1667 años
+
+    tarifaPromedio = round ( (dataFrame['fare'].mean()) , 2 )
+    # print ('La tarifa promedio es: ', tarifaPromedio, 'usd')
+    # La tarifa promedio es:  33.3 usd
+    
+    diccionario = dict()
+    diccionario['nombres'] = list(nombres[:10])
+    diccionario['edadMayor'] = mayorEdad
+    diccionario['edadMenor'] = menorEdad
+    diccionario['tarifaPromedio'] = tarifaPromedio
+    
+    return diccionario
+
+print (funcionEjemplo ('titanic3.csv'))
+
+# {'nombres': ['Allen, Miss. Elisabeth Walton', 'Allison, Master. Hudson Trevor', 'Allison, Miss. Helen Loraine', 'Allison, Mr. Hudson Joshua Creighton', 'Allison, Mrs. Hudson J C (Bessie Waldo Daniels)', 'Anderson, Mr. Harry', 'Andrews, Miss. Kornelia Theodosia', 'Andrews, Mr. Thomas Jr', 'Appleton, Mrs. Edward Dale (Charlotte Lamson)', 'Artagaveytia, Mr. Ramon'], 'edadMayor': 80.0, 'edadMenor': 0.1667, 'tarifaPromedio': 33.3}
--- _44semana06clase18archivos.py
+++ _44semana06clase18archivos.py
@@ -76,7 +76,6 @@ def ejemplo1TXT():
       fic.write(linea)
       fic.write("\n")
   fic.close()
-
 ejemplo1TXT()
 
 
@@ -96,3 +95,54 @@ def ejemplo3TXT():
   print (lineas)
   fic.close()
 ejemplo3TXT()
+
+def ejemplo1JSON():
+  import json
+
+  data = {}
+  data['clients'] = []
+
+  data['clients'].append({
+      'first_name': 'Sigrid',
+      'last_name': 'Mannock',
+      'age': 27,
+      'amount': 7.17})
+
+  data['clients'].append({
+      'first_name': 'Joe',
+      'last_name': 'Hinners',
+      'age': 31,
+      'amount': [1.90, 5.50]})
+
+  data['clients'].append({
+      'first_name': 'Theodoric',
+      'last_name': 'Rivers',
+      'age': 36,
+      'amount': 1.11})
+
+  with open('archivo6.json', 'w') as file:
+      json.dump(data, file, indent=4)
+ejemplo1JSON()
+
+def ejemplo2JSON():
+  import json
+  with open('archivo6.json') as file:
+    data = json.load(file)
+
+    for client in data['clients']:
+        print('First name:', client['first_name'])
+        print('Last name:', client['last_name'])
+        print('Age:', client['age'])
+        print('Amount:', client['amount'])
+        print('')
+ejemplo2JSON()
+
+def ejemplo3JSON():
+  import json
+  import requests
+
+  resp = requests.get('http://ip-api.com/json/208.80.152.201')
+  json.loads(resp.content)
+ejemplo3JSON()
+
+
